5b8f0a1b9ebc8aec9609d44907e9e6152f533cba
Juan Mahecha <juanmahecha@iMac-de-Juan.local>
2021-09-15 12:39:18 -0500

    configuracion proxy y script for run

--- create-project.sh
+++ create-project.sh
@@ -23,6 +23,9 @@ function angularInit(){
     cd $projectNameDirectory
     echo "Make a project"
     ng new $projectNameDirectory --style=scss -p a6s --routing true
+    # create file proxy conf
+    cd $projectNameDirectory
+    curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631667640/scripts/proxy.conf.json
 }
 
 function scripts(){
@@ -36,13 +39,13 @@ function createShFiles(){
    echo ".sh files was added to scripts box"
    #Estos archivos por practicidad y para evetirar errores a la hora de ejecución se cargaron en una nube, lo cual los permite descagar
    curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631665657/scripts/services.sh
-   curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631665657/scripts/run.sh
    curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631665657/scripts/readMe.md
    curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631665657/scripts/git-remove.sh
    curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631665657/scripts/guard.sh
    curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631665657/scripts/common-components.sh
    curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631665657/scripts/componets.sh
    curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631665657/scripts/class.sh
+   curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631727489/scripts/run.sh
 }
 
 main
