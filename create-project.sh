e4678dce256889c66175f83439ef03343806b062
juanmahecha.9 <juanmahecha.dc@gmail.com>
2022-01-12 17:51:49 -0500

    cambio

--- create-project.sh
+++ create-project.sh
@@ -40,6 +40,10 @@ function angularInit(){(
     sleep 2
     bootstrap
     echo "bootstrap dependencies was intalled"
+    sleep 2
+    angularMaterial
+    echo "angular material dependencies was intalled"
+    sleep 2
 )}
 
 function scripts(){(
@@ -59,7 +63,7 @@ function createShFiles(){(
    curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631665657/scripts/common-components.sh
    curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631807053/scripts/components.sh
    curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631665657/scripts/class.sh
-   curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631727489/scripts/run.sh
+   curl -O https://res.cloudinary.com/yuem1/raw/upload/v1642025716/scripts/run.sh
    curl -O https://res.cloudinary.com/yuem1/raw/upload/v1632945626/scripts/modules.sh
 )}
 
@@ -76,6 +80,13 @@ function bootstrap(){(
     npm i bootstrap jquery @popperjs/core
 )}
 
+function angularMaterial(){(
+    cd $projectNameDirectory
+    ### instalar bootstrap
+    sleep 2
+    ng add @angular/material
+)}
+
 function files(){(
     rm readMe.md
     curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631803567/scripts/readme%20angular/readMe.md
@@ -83,6 +94,27 @@ function files(){(
     mkdir bootstrap-info
     cd bootstrap-info
     curl -O https://res.cloudinary.com/yuem1/raw/upload/v1631804273/scripts/bootstrap-info.txt
+)}
+
+function downloadImg(){(
+    cd src/assets
+    mkdir img
+    cd img
+    curl -O https://res.cloudinary.com/yuem1/image/upload/v1618457945/LOGO_YUEM.jpg
+)}
+
+function removeInfo(){(
+    cd src/app
+    cat /dev/null > app.component.html
+    #Colocar nueva informacion
+    sleep 1
+    tee -a app.component.html << EOF
+<h1 style="text-align: center;">Created by:</h1>
+<h3 style="text-align: center;"><a href="https://github.com/juanmahecha9" target="_blank">Juanmahecha9</a></h3>
+<hr>
+<br>
+<img style="display:block; margin:auto; " src="../assets/img/LOGO_YUEM.jpg" alt="Logo juanmahecha9">
+EOF
 )}
 
 # Main function.
@@ -105,6 +137,13 @@ function main(){
     # New ReadMe
     files
 
+    #download img
+    downloadImg
+    sleep 1
+
+    # Eliminar informacion del archivo html principal
+    removeInfo
+    sleep 1
     # Delete the first git repository that was create at the moment to create the angular project
     sh scripts/git-remove.sh   
     echo " "
